b8d9abb890161f3d9d51d8eeba3b77a60c6e49ee
Ederson Torresini <boidacarapreta@gmail.com>
2023-08-21 20:18:09 +0000

    Adicionar 1 cena e 1 imagem

---
+++ cena0.js
@@ -0,0 +1,16 @@
+export default class cena0 extends Phaser.Scene {
+
+  constructor () {
+    super('cena0')
+  }
+
+  preload() {
+    this.load.image('ifsc-sj-2014', './ifsc-sj-2014.png')
+  }
+
+  create(){
+    this.add.image(400, 225, 'ifsc-sj-2014')
+  }
+
+  update(){}
+}
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /* Importação de objetos */
 import config from './config.js'
+import cena0 from './cena0.js'
 
 /* Definição do objeto Game */
 class Game extends Phaser.Game {
   constructor () {
     super(config)
+    
+    this.scene.add('cena0', cena0)
+    this.scene.start('cena0')
   }
 }
 
