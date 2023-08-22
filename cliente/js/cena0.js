975f6df7119cfb97e91053dc02e3a5932f52bc0c
Ederson Torresini <boidacarapreta@gmail.com>
2023-08-22 20:19:14 +0000

    Organizar código em pastas

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -1,16 +1,15 @@
 export default class cena0 extends Phaser.Scene {
-
   constructor () {
     super('cena0')
   }
 
-  preload() {
-    this.load.image('ifsc-sj-2014', './ifsc-sj-2014.png')
+  preload () {
+    this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
   }
 
-  create(){
+  create () {
     this.add.image(400, 225, 'ifsc-sj-2014')
   }
 
-  update(){}
+  update () { }
 }
--- cliente/js/index.js
+++ cliente/js/index.js
@@ -6,7 +6,7 @@ import cena0 from './cena0.js'
 class Game extends Phaser.Game {
   constructor () {
     super(config)
-    
+
     this.scene.add('cena0', cena0)
     this.scene.start('cena0')
   }
